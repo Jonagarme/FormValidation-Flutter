50bd71abb463a4f0b6f9aadbf1dc142046d0d844
Jonagarme <garciajonathan268@gmail.com>
2021-04-30 18:47:33 -0500

    grabar token en el dispositivo

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:form_validation/src/bloc/login_bloc.dart';
 import 'package:form_validation/src/bloc/provider.dart';
+import 'package:form_validation/src/providers/usuario_provider.dart';
 
 class LoginPage extends StatelessWidget {
+  final usuarioProvider = new UsuarioProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -136,12 +139,9 @@ class LoginPage extends StatelessWidget {
   }
 
   _login(LoginBloc bloc, BuildContext context) {
-    print('===============');
-    print('Email: ${bloc.email}');
-    print('Passord: ${bloc.password}');
-    print('===============');
+    usuarioProvider.login(bloc.email, bloc.password);
 
-    Navigator.pushReplacementNamed(context, 'home');
+    //Navigator.pushReplacementNamed(context, 'home');
   }
 
   Widget _crearFondo(BuildContext context) {
--- lib/src/providers/usuario_provider.dart
+++ lib/src/providers/usuario_provider.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 
+import 'package:form_validation/src/preferencias_usuario/preferencias_usuario.dart';
 import 'package:http/http.dart' as http;
 
 class UsuarioProvider {
-  final String firebaseToken = 'AIzaSyCFkqdPT3_zLktMcAXko1qJzeUkSPK6_Bg';
+  final String _firebaseToken = 'AIzaSyCFkqdPT3_zLktMcAXko1qJzeUkSPK6_Bg';
+  final _prefs = new PreferenciasUsuario();
 
-  Future nuevoUsuario(String email, String password) async {
+  Future<Map<String, dynamic>> login(String email, String password) async {
     final authData = {
       'email': email,
       'password': password,
@@ -13,7 +15,7 @@ class UsuarioProvider {
     };
 
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$firebaseToken');
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$_firebaseToken');
 
     final resp = await http.post(url, body: json.encode(authData));
 
@@ -22,7 +24,34 @@ class UsuarioProvider {
     print(decodeResp);
 
     if (decodeResp.containsKey('idToken')) {
-      //TODO: salvar el token en el storage
+      _prefs.token = decodeResp['idToken'];
+
+      return {'ok': true, 'token': decodeResp['idToken']};
+    } else {
+      return {'ok': false, 'mensaje': decodeResp['error']['message']};
+    }
+  }
+
+  Future<Map<String, dynamic>> nuevoUsuario(
+      String email, String password) async {
+    final authData = {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true
+    };
+
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$_firebaseToken');
+
+    final resp = await http.post(url, body: json.encode(authData));
+
+    Map<String, dynamic> decodeResp = json.decode(resp.body);
+
+    print(decodeResp);
+
+    if (decodeResp.containsKey('idToken')) {
+      _prefs.token = decodeResp['idToken'];
+
       return {'ok': true, 'token': decodeResp['idToken']};
     } else {
       return {'ok': false, 'mensaje': decodeResp['error']['message']};
