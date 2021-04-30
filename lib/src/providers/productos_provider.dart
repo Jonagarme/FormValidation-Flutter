a7d3a48c73923e02dfe634611d3037532b18b4c3
Jonagarme <garciajonathan268@gmail.com>
2021-04-30 12:12:52 -0500

    subir fotos desde flutter

--- lib/src/providers/productos_provider.dart
+++ lib/src/providers/productos_provider.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:mime_type/mime_type.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:form_validation/src/models/producto_model.dart';
 
 class ProductosProvider {
@@ -58,5 +61,32 @@ class ProductosProvider {
     print((resp.body));
 
     return 1;
+  }
+
+  Future<String> subirImagen(File imagen) async {
+    final url = Uri.parse(
+        'https://api.cloudinary.com/v1_1/dznazxnhx/image/upload?upload_preset=k9wva9av');
+
+    final mimeType = mime(imagen.path).split('/'); //image/jpeg
+
+    final imageUploadRequest = http.MultipartRequest('POST', url);
+
+    final file = await http.MultipartFile.fromPath('file', imagen.path,
+        contentType: MediaType(mimeType[0], mimeType[1]));
+    imageUploadRequest.files.add(file);
+
+    final streamResponse = await imageUploadRequest.send();
+    final resp = await http.Response.fromStream(streamResponse);
+
+    if (resp.statusCode != 200 && resp.statusCode != 201) {
+      print('Algo salio mal');
+      print(resp.body);
+      return null;
+    }
+
+    final respData = json.decode(resp.body);
+    print(respData);
+
+    return respData['secure_url'];
   }
 }
